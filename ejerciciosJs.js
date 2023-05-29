e88a91cbf6d0c9bdaf5dff5a5a40c56f77f9543c
Fabio <aringolifabio@gmail.com>
2023-05-29 10:29:11 -0300

    first commit

---
+++ ejerciciosJs.js
@@ -0,0 +1,9 @@
+let miNombre = "Fabio"
+let miApellido = "Aringoli"
+let miEdad = 24
+let miMascota = "Branca"
+let edadMascota = 13
+let nombreCompleto = miNombre + " " + miApellido 
+let textoPresentacion = "Hola mi nombre es " + nombreCompleto + ", tengo " + miEdad + " años, mi mascota se llama " + miMascota + " Y tiene " + edadMascota + " años y es lo mas grande que hay"
+
+console.log (textoPresentacion)
